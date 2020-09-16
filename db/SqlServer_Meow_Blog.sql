90c12805e568f796b19181c70531d205c8a68f74
idkook <985609+idkook@users.noreply.github.com>
2020-09-16 13:43:04 +0800

    Initialize v_3.0.0

---
+++ db/SqlServer_Meow_Blog.sql
@@ -0,0 +1,87 @@
+﻿USE [Meowv_Blog]
+GO
+/****** Object:  Table [dbo].[Meowv_Tags]    Script Date: 04/20/2019 19:10:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Meowv_Tags](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[TagName] [nvarchar](20) NOT NULL,
+	[CreationTime] [datetime] NOT NULL,
+ CONSTRAINT [PK_Meowv_Tags] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Meowv_Categories]    Script Date: 04/20/2019 19:10:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Meowv_Categories](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](20) NOT NULL,
+	[CreationTime] [datetime] NOT NULL,
+ CONSTRAINT [PK__meowv_ca__3214EC07117F9D94] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = OFF, ALLOW_PAGE_LOCKS  = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Meowv_ArticleTags]    Script Date: 04/20/2019 19:10:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Meowv_ArticleTags](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ArticleId] [int] NOT NULL,
+	[TagId] [int] NOT NULL,
+ CONSTRAINT [PK__meowv_ar__3214EC0707020F21] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = OFF, ALLOW_PAGE_LOCKS  = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Meowv_Articles]    Script Date: 04/20/2019 19:10:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Meowv_Articles](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](100) NOT NULL,
+	[Author] [nvarchar](10) NOT NULL,
+	[Source] [nvarchar](20) NOT NULL,
+	[Url] [nvarchar](255) NOT NULL,
+	[Summary] [nvarchar](200) NOT NULL,
+	[Content] [nvarchar](max) NOT NULL,
+	[Hits] [int] NOT NULL,
+	[MetaKeywords] [nvarchar](100) NOT NULL,
+	[MetaDescription] [nvarchar](200) NOT NULL,
+	[CreationTime] [datetime] NOT NULL,
+	[PostTime] [datetime] NOT NULL,
+	[IsDeleted] [bit] NOT NULL,
+ CONSTRAINT [PK_Meowv_Articles] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Meowv_ArticleCategories]    Script Date: 04/20/2019 19:10:00 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Meowv_ArticleCategories](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[ArticleId] [int] NOT NULL,
+	[CategoryId] [int] NOT NULL,
+ CONSTRAINT [PK_Meowv_Articlecategories] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
---
+++ src/MeowvBlog.SOA/wwwroot/js/tags.js
@@ -0,0 +1,13 @@
+// ·ÖÀàÁÐ±í
+var parameter = {
+    url: "/tag/get",
+    callback: function (data) {
+        if (data.isSuccess) {
+            document.title = "Tags - " + document.title;
+
+            var html = template("tags_tmpl", data);
+            document.getElementById('tags').innerHTML = html;
+        }
+    }
+};
+_ajax(parameter);
